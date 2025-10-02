a227396546ab74e5c3243ee69fb30bede281e003
Peter Sciranka <peter@Peters-MacBook-Pro-4.local>
2025-10-02 21:26:57 +0200

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+const repo = process.env.GITHUB_REPO || ''
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: repo ? `/${repo}` : '',
+}
 
-export default nextConfig;
+export default nextConfig
